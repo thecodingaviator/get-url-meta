8aed2eeacf265bf14a0c198725e380a3ae560c1d
Glitch (thecodingaviator-get-url-meta-2) <none>
2020-07-08 09:38:44 +0000

    🍕👼 Checkpoint ./meta.js:2314123/1966 ./package.json:2314123/1362

--- meta.js
+++ meta.js
@@ -1,42 +1,42 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const urlMetadata = require('url-metadata')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const urlMetadata = require("url-metadata");
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.get('/api/getMeta', (req, res) => {
-    urlMetadata('https://bit.ly/2ePIrDy').then(
-        function (metadata) {
-            res.json(JSON.stringify(metadata))
-        },
-        function (error) {
-            res.status(404).json({message: error})
-        }
-    )
-})
+app.get("/api/getMeta", (req, res) => {
+  urlMetadata("https://bit.ly/2ePIrDy").then(
+    function(metadata) {
+      res.json(JSON.stringify(metadata));
+    },
+    function(error) {
+      res.status(404).json({ message: error });
+    }
+  );
+});
 
-app.get('/api/getMeta/:url', (req, res) => {
-    const url = req.params.url
-    console.log('yes')
-    console.log(url)
-    urlMetadata('https://'+url).then(
-        function (metadata) {
-            res.json(JSON.stringify(metadata))
-        },
-        function (error) {
-            res.status(404).json({message: error})
-        }
-    )
-})
+app.get("/api/getMeta/:url", (req, res) => {
+  const url = req.params.url;
+  console.log("yes");
+  console.log(url);
+  urlMetadata("https://" + url).then(
+    function(metadata) {
+      res.json(JSON.stringify(metadata));
+    },
+    function(error) {
+      res.status(404).json({ message: error });
+    }
+  );
+});
 
-const hostname = 'localhost'
-const port = process.env.PORT
+const hostname = "localhost";
+const port = process.env.PORT || 9000;
 
 app.listen((port, hostname) => {
-    console.log('Running')
-})
+  console.log("Running");
+});
